e7315e16fca24e739e25254465fd31aa7597d859
La Vinia Parker <laviniaparker@La-Vinias-MacBook-Pro.local>
2024-01-02 16:57:11 -0500

    sortedarray

--- Easy/3. SortedSquaredArray.js
+++ Easy/3. SortedSquaredArray.js
@@ -1,7 +1,7 @@
-Write a function that takes in a non-empty array of integers that are sorted in ascending order and returns a new array of the same length with the squares of the original integers also sorted in ascending
+// Write a function that takes in a non-empty array of integers that are sorted in ascending order and returns a new array of the same length with the squares of the original integers also sorted in ascending
 
-Sample Input
-array = [1, 2, 3, 5, 6, 8, 9]
+// Sample Input
+// array = [1, 2, 3, 5, 6, 8, 9]
 
-Sample Output
-[1, 4, 9, 25, 36, 64, 81]
+// Sample Output
+// [1, 4, 9, 25, 36, 64, 81]
