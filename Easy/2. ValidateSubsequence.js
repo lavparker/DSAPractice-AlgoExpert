6ca7187deb28cc7bb9fb587df0889262fe49fbb0
La Vinia Parker <laviniaparker@La-Vinias-MacBook-Pro.local>
2024-01-02 11:55:45 -0500

    validate subsequence

---
+++ Easy/2. ValidateSubsequence.js
@@ -0,0 +1,15 @@
+function isValidSubsequence(array, sequence) {
+  //need to return true or false 
+  //iterate through the main array 
+  // check if the number in the subseq is equal to the num in the array, if not, 
+  // move to the next num in the main array, 
+  //if so, move to the next num in the subarr, if the character you're on in the subsequence is 
+  // === to sub array.length - 1, return 
+
+  for(let i = 0; i < array.length; i++){
+    if(array[i] === sequence[i]){
+      
+    }
+  }
+  
+}
