5df290ac7f721dc591d9e99500e7e2236259684f
La Vinia Parker <laviniaparker@La-Vinias-MacBook-Pro.local>
2024-01-02 14:13:23 -0500

    solved findclosestvalueinbst

---
+++ Easy/7. FindClosestValueInBST.js
@@ -0,0 +1,42 @@
+// Write a function that takes in a Binary Search Tree (BST) and a target integer value and returns the closest value to that target value contained in the BST.
+// You can assume that there will only be one closest value.
+// Each
+// BST
+// node has an integer value, a left child node, and a right child node. A node is said to be a valid BST node if and
+// only if it satisfies the BST property: its value is strictly greater than the values of every node to its left; its value is less than or equal to the values of every node to its right; and its children nodes are either valid BST nodes themselves or None / null.
+
+
+// tree =   10
+//         /   \
+//        5     15
+//       / \    /  \
+//     2    5  13   22
+//    /          \
+//   1            14 
+
+
+// target = 12
+
+
+
+function findClosestValueInBst(tree, target) {
+  return findClosestValueInBSTHelper(tree, target, Infinity)
+}
+
+function findClosestValueInBSTHelper(tree, target, closest){
+  if (tree === null){
+    return closest
+  }
+
+  if (Math.abs(target - closest) > Math.abs(target - tree.value)){
+    closest = tree.value
+  }
+
+  if (target < tree.value){
+      return findClosestValueInBSTHelper(tree.left, target, closest)
+  }else if(target > tree.value){
+    return findClosestValueInBSTHelper(tree.right, target, closest)
+  }else {
+    return closest
+  }
+}
