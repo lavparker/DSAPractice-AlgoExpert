aed15a114455555ba04f02af31159c9a85032e91
La Vinia Parker <lavinia.parker@outlook.com>
2024-01-18 16:08:30 -0500

    completed monotonic array and created spiraltraverse

---
+++ Medium/SpiralTraverse.js
@@ -0,0 +1,10 @@
+//savebounds to variables , start row, endrow, startcol, endcol 
+//right, incrementing, hit last column 
+// down, incrementing, not including first elel, hit last row
+// back, decrementing not including rirst Element, hit firtsn col 
+// up, decrement, not including the first element,, hit, and NOT INCLUDING first row, 
+// move bounds in by 1
+// stop when endRow >= startRow or when endCol >= startcol 
+
+//return: Id array of the elements in spiral order 
+//
