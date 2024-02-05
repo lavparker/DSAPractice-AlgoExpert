b8d406c9b66b061f5bb0f5b9abd27dba9eddfad0
La Vinia Parker <lavinia.parker@outlook.com>
2024-02-05 11:59:46 -0500

    solved Common Characters

--- Easy/CommonCharacters.js
+++ Easy/CommonCharacters.js
@@ -1,3 +1,13 @@
+Common Characters
+Write a function that takes in a non-empty list of non-empty strings and returns a list of characters that are common to all strings in the list, ignoring multiplicity.
+Note that the strings are not guaranteed to only contain alphanumeric characters. The list you return can be in any order.
+
+Sample Input
+strings = ["abc", "bcd", "baccd" ]
+Sample Output
+["b", "c"] // The characters could be ordered
+d
+
 function commonCharacters(strings){
   let common = [];
   let fstWrd = strings[0];
@@ -34,6 +44,58 @@ function sameChar(restStr, letter){
 //take the letter that you've iterated to and check that each element in the array .includes() in the other string
 //if true for every string common.push the letter
 //return common
+
+// Do not edit the line below.
+exports.commonCharacters = commonCharacters;
+
+
+function commonCharacters(strings) {
+  // Write your code here.
+  let common = [];
+  let firstStr = strings[0];
+  let restStr = strings.slice(1);
+
+  if(strings.length <= 1){
+    return strings
+  }
+
+  for(let i = 0; i < firstStr.length; i++){
+    let char = firstStr[i];
+    if(otherStrs(restStr, char)){
+      if(!common.includes(char)){
+      common.push(char)
+      }
+    }
+
+  }
+  return common;
+}
+
+function otherStrs(restStr, char){
+  for(let j = 0; j < restStr.length; j++){
+    if(!restStr[j].includes(char)){
+      return false;
+    }
+  }
+  return true;
+}
+
+//create a variable called common and set it equal to an empty array
+//create a variable called firstStr and set it equal to strings[0]
+//create a variable called restStr and set it equal to strings.slice(1);
+//loop through firstStr
+//create a variable called char and set it equal to firstStr[i];
+//call helper function
+//if helper function is true, push letter to common array
+//create a helper function and loop through restStr //parameter is restStr and letter
+//check to see if letter.include() in the other strings. if true for all,
+//return common array
+
+//edge cases :
+// what do you do if the string of arrays is only one element long
+// what do you do if the "strings all have the same letters "
+//Time Complexity: O(m * n * k)
+//Space Complexity: O(m)
 
 // Do not edit the line below.
 exports.commonCharacters = commonCharacters;
