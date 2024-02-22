19ed27ffef2fb61b3d957868e4104c40f577875f
La Vinia Parker <lavinia.parker@outlook.com>
2024-02-22 11:21:05 -0500

    solved sunsetviews

--- Medium/SunsetViews.js
+++ Medium/SunsetViews.js
@@ -12,3 +12,61 @@
 // [1, 3, 6, 7]
 
 
+function sunsetViews(buildings, direction) {
+  // Write your code here.
+
+  // let sort = ((a, b) => a - b);
+  let highest = 0;
+  let view = [];
+
+  if (direction === 'WEST'){
+    for(let i = 0; i < buildings.length; i++){
+      let b = buildings[i];
+      if(b > highest){
+        // console.log(b, " is greater than ", highest, " index is ", i)
+        highest = b
+        view.push(i)
+      }
+    }
+  }
+
+  if(direction === 'EAST'){
+    for(let i = buildings.length - 1; i >= 0; i--){
+      let b = buildings[i];
+      if(b > highest){
+        // console.log("i've entered the east loop")
+        // console.log(b, " is greater than ", highest, " index is ", i)
+        highest = b
+        view.push(i)
+      }
+    }
+  }
+  return view.sort((a, b) => a - b);
+}
+
+//will be given buildings and directions
+//need to return array of indicies of the buildings that can see the sunset
+
+//can see the sunset if it's taller than all of the buildings that come after it in the direction it faces
+
+
+// create a variable called sort...set it equal to the sort function
+
+//create a variable called highest and set it equal to 0;
+//create a variable called view and set it equal to []
+//if buildings are facing east, you need to iterate through the loop backwards
+//if buildings are facing west, you need to iterate through the loop forwards
+
+//set a variable called b1 = building[i]
+
+
+//WEST
+//if b1 > highest , view.push(i), highest = b1
+
+
+
+//EAST
+//if b1 > highest , view.push(i), highest = b1
+
+// Do not edit the line below.
+exports.sunsetViews = sunsetViews;
