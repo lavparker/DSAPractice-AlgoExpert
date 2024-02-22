3d6a7f99ffbe5f2778f443a1f6e9258043ab6ea2
La Vinia Parker <lavinia.parker@outlook.com>
2024-02-22 10:21:51 -0500

    minmaxstack attempt

--- Medium/MinMaxStackConstruction.js
+++ Medium/MinMaxStackConstruction.js
@@ -22,3 +22,49 @@
 // 5
 // peek(): 5
 
+// Feel free to add new properties and methods to the class.
+class MinMaxStack {
+  constructor(){
+    this.array = []
+  }
+  peek(array) {
+    if(this.length === 0){
+      return null
+    }else{
+      return this.array[this.array.length - 1]
+    }
+
+  }
+
+  pop(array) {
+    // Write your code here.
+   return this.array.pop()
+  }
+
+  push(number) {
+    // Write your code here.
+    return this.array.push(number)
+  }
+
+  getMin(array) {
+    // Write your code here.
+    return Math.min(this.array)
+  }
+
+  getMax(array) {
+    // Write your code here.
+    return Math.max(this.array)
+  }
+}
+
+//Peek - if this.length === 0return null else return array[0]
+
+//Pop - if array.isEmpty?() return null else
+  //create a variable called popped and set it equal to array.pop()
+  //return popped
+
+//Push - this.push()
+//Min
+//Max
+// Do not edit the line below.
+exports.MinMaxStack = MinMaxStack;
