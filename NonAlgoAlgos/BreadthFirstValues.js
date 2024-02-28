02f280fb7a35ee01a60fcdb83911110878a2b8d6
La Vinia Parker <lavinia.parker@outlook.com>
2024-02-28 18:14:56 -0500

    completed breadth first values iterative solution

--- NonAlgoAlgos/BreadthFirstValues.js
+++ NonAlgoAlgos/BreadthFirstValues.js
@@ -82,3 +82,45 @@
 // //    -> ['a', 'b', 'c', 'x', 'd', 'e']
 // breadthFirstValues(null);
 // //    -> []
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+const breadthFirstValues = (root) => {
+  //iterative approach
+
+  //want to check if our root is null
+
+  if(root === null) return [];
+
+  //start with queue data structure
+  const queue = [root];
+
+  //next, I need to create my main algorithm
+  while(queue.length > 0){
+    //begin algorithm by removing from the front of my queue.
+    //I will remove from one end and add to the opposite end. I will         treat index 0 as the front of my array and the last index as if it's     the back of my array
+    const values = [];
+    const current = queue.shift();
+    //as something leaves the queue I want to push it to values.
+    values.push(current.val);
+    //from here I need to add current's children into my queue.
+    if(current.left !== null) queue.push(current.left);
+    if(current.right !== null) queue.push(current.right);
+
+
+  }
+
+  //once my while loop is done running I will return my entire values array once
+  return values;
+
+};
+
+module.exports = {
+  breadthFirstValues,
+};
