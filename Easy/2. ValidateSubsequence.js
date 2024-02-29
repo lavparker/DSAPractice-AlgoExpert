a87933775810b74aed9ace4dcb22ebdd4b551a8f
La Vinia Parker <lavinia.parker@outlook.com>
2024-02-29 14:15:24 -0500

    attempted validate subsequence

--- Easy/2. ValidateSubsequence.js
+++ Easy/2. ValidateSubsequence.js
@@ -1,15 +1,47 @@
-function isValidSubsequence(array, sequence) {
-  //need to return true or false 
-  //iterate through the main array 
-  // check if the number in the subseq is equal to the num in the array, if not, 
-  // move to the next num in the main array, 
-  //if so, move to the next num in the subarr, if the character you're on in the subsequence is 
-  // === to sub array.length - 1, return 
+// Validate Subsequence
+// Given two non-empty arrays of integers, write a function that determines whether the second array is a subsequence of the first one.
+// A subsequence of an array is a set of numbers that aren't necessarily adjacent in the array but that are in the same order as they appear in the array. For instance, the numbers [1, 3, 4] form a subsequence of the array [1, 2, 3, 41, and so do the numbers [2, 4] . Note that a single number in an array and the array itself are both valid subsequences of the array.
+// Sample Input
+// array = [5, 1, 22, 25, 6, -1, 8, 10]
+// sequence = [1, 6, -1, 10]
+// Sample Output
+// true
 
-  for(let i = 0; i < array.length; i++){
-    if(array[i] === sequence[i]){
-      
+
+// Instead of comparing arrays directly, you should compare their content. You can do this by converting both arrays to strings using JSON.stringify() and then comparing the resulting strings.
+
+function isValidSubsequence(array, sequence) {
+  let seqArr = [];
+
+  for(let i = 0; i < sequence.length; i++){
+    if(array.indexOf(sequence[i]) === -1){
+      return false
+    }else{
+      seqArr.push(array.indexOf(sequence[i]))
+
     }
   }
-  
+
+  let sortedArr = seqArr.sort((a, b) => a - b);
+  // let sortedSet = new Set(sortedArr);
+  // let sortedSetArr = Array.from(sortedSet);
+  console.log("sortedSetArr ", sortedSetArr, "seqArr ", seqArr)
+
+  return JSON.stringify(sortedSetArr) === JSON.stringify(seqArr);
 }
+
+// Do not edit the line below.
+exports.isValidSubsequence = isValidSubsequence;
+
+//need to determine if the second array (sequence), is a subsequence of the first one(subsequence);
+
+//create a variable called seqArry and set it equal to an empty array
+
+//loop thrugh sequence array.
+//use .indexOf to find the index of the number in the array
+//if indexOf returns -1 return false.
+//if index of returns a number > -1, push it into seqArray
+//once all of the nums are pushed.
+//create a variable called sortedArr and set it equal to the sorted version of the seqArr
+//return true if the sorteArr is equal to the seqArr
+//else false 
