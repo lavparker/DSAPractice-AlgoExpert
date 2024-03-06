8c3027068cd59f618724def5e556f37c770736aa
La Vinia Parker <lavinia.parker@outlook.com>
2024-03-06 15:33:54 -0500

    solved majority element

--- LeetCode/MajorElement.js
+++ LeetCode/MajorElement.js
@@ -28,3 +28,45 @@
 // Follow-up: Could you solve the problem in linear time and in O(1) space?
 
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let countNum = {};
+
+    for(let i = 0; i < nums.length; i++){
+        let num = nums[i];
+
+        if(countNum[num]){
+            countNum[num] ++
+        }else{
+            countNum[num] = 1;
+        }
+    }
+
+    let numCountArr = Object.entries(countNum);
+    let count = 0;
+    let freqNum;
+
+    for(let j = 0; j < numCountArr.length; j++){
+        let hashVar = numCountArr[j]
+        if(hashVar[1] > count){
+            count = hashVar[1];
+            freqNum = hashVar[0];
+        }
+    }
+
+    return freqNum;
+};
+
+//create a variable called countNum and set it equal to an empty hash
+//loop through nums array
+//add the count of each number to the hash
+
+//create a counter variable and set it equal to 0
+//create a variable called freqNum
+//create a variable and set it equal to object.enttries(hash)
+//loop through this variable and check to see if hashVar[1] > count
+//if yes,  count = hashVar[1] && freqNum = hashVar[0]
+//return freqNum 
