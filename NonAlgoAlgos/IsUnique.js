ddc467ed48a35d47be5240b90bb616063267cac1
La Vinia Parker <lavinia.parker@outlook.com>
2024-03-06 11:06:58 -0500

    created isUnique algo

---
+++ NonAlgoAlgos/IsUnique.js
@@ -0,0 +1,13 @@
+// Implement an algorithm to determine if a string has allunique characters. What if you cannot use additional data structures?
+
+
+function isUnique(){
+
+}
+
+
+const string = "DSA is Fun!"
+//expected: false
+
+const str = "I don't rupeax"
+//expeat: true 
